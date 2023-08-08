f72dfc545b12a5cd502492429f9533533913701e
DavidJ3562 <hys0305060273@gmail.com>
2023-08-08 10:11:43 +0300

    fixed rpc url

--- xenft-creator.py
+++ xenft-creator.py
@@ -47,7 +47,7 @@ how_many_seconds_between_checks = 10
 
 # For RPC node you can use Infura URL. Note: All requests to Infura must have a valid API key appended to the request URL or they will fail.
 # Get your own at: https://infura.io (Video Guide: https://youtu.be/R2WkpF4Em7k)
-rpc_url = f"https://mainnet.infura.io/v3/{INFURA_ID}"  # Replace ABCDEF with your Infura API id
+rpc_url = f"https://polygon-mainnet.infura.io/v3/{INFURA_ID}"  # Replace ABCDEF with your Infura API id
 # Alternatively, if you don't want to sign up with Infura and monitor your own requests, just uncomment the line below to use Public Ethereum RPC, or get one from: https://llamanodes.com/public-rpc
 # rpc_url = "https://eth.llamarpc.com"
 
@@ -59,7 +59,10 @@ your_wallet_address = PUBLIC_KEY  # replace with the account from which we'll pa
 
 # PRIVATE KEYS
 # RECOMMENDED APPROACH: Script will ask you to enter the private key manually (at runtime)
-your_wallet_address_private_key = getpass.getpass(prompt="Please enter your wallet private key: ")
+
+# your_wallet_address_private_key = getpass.getpass(prompt="Please enter your wallet private key: ")
+your_wallet_address_private_key = os.environ['PRIVATE_KEY']
+
 # NOT SECURE & NOT RECOMMENDED: Uncomment the following line to hardcode your wallet's private key
 # your_wallet_address_private_key = '012345abcdef...'
 
@@ -77,8 +80,8 @@ xen_abi = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"
 
 ## ----------- FUNCTIONS START (DON'T TOUCH) ----------- ##
 
-# Get Ethereum cost in USD
-def get_eth_usd_value():
+# Get Matic cost in USD
+def get_matic_usd_value():
     cg = CoinGeckoAPI()
     price = cg.get_price(ids=['matic-network'], vs_currencies='usd')
     usd_value = price['matic-network']['usd']
@@ -98,7 +101,7 @@ def fetch_current_max_term(xen_address):
     # Uncomment if you want to fetch ABI directly from the contract, otherwise leave commented
     #xen_abi = fetch_abi(address)
     xen_contract = web3.eth.contract(address=address, abi=xen_abi)
-    current_max_term = int(xen_contract.functions.getCurrentMaxTerm().call() / (60*60*24))
+    current_max_term = int(xen_contract.functions.getCurrentMaxTerm().call() / (60 * 60 * 24))
     #print(f"Setting the XEN maximum term to: {current_max_term}")
     return current_max_term
 
@@ -126,14 +129,13 @@ def get_gas_price():
 
 ## ----------- BODY OF THE PROGRAM - START (DON'T TOUCH) ----------- ##
 
-for i in range(1, num_of_xenft_to_mint+1):
+for i in range(1, num_of_xenft_to_mint + 1):
 
     # Uncomment if you want to fetch ABI directly from the contract, otherwise leave commented
     #xenft_abi = fetch_abi(xenft_contract_address)
     # Uncomment if you want to fetch ABI directly from the contract, otherwise leave commented
     #xen_abi = fetch_abi(xen_contract_address)
 
-    get_eth_usd_value()
     if use_automatic_max_term:
         term = fetch_current_max_term(xen_contract_address) # This will fetch the current MAX TERM automatically from XEN smart contract.
     else:
@@ -167,11 +169,9 @@ for i in range(1, num_of_xenft_to_mint+1):
             else:
                 time.sleep(how_many_seconds_between_checks)
 
-    eth_cost_in_usd = get_eth_usd_value()
-    balance_before_claim = round(float(web3.fromWei(web3.eth.getBalance(your_wallet_address), 'ether')),6)
-    total_account_value = round(eth_cost_in_usd * balance_before_claim,2)
-
-
+    matic_cost_in_usd = get_matic_usd_value()
+    balance_before_claim = round(float(web3.fromWei(web3.eth.getBalance(your_wallet_address), 'ether')), 6)
+    total_account_value = round(matic_cost_in_usd * balance_before_claim, 2)
 
     # Build the transaction
     try:
@@ -185,9 +185,9 @@ for i in range(1, num_of_xenft_to_mint+1):
         tx['gas'] = gas
 
         if web3.eth.getBalance(your_wallet_address) < gas*maxFeePerGas:
-            raise Exception("Not enough Ether in your wallet address to pay for the transaction")
+            raise Exception("Not enough MATIC in your wallet address to pay for the transaction")
         else:
-            print(f"{get_timestamp()} - Account balance before XENFT claim: {balance_before_claim} ETH (${total_account_value})")
+            print(f"{get_timestamp()} - Account balance before XENFT claim: {balance_before_claim} MATIC (${total_account_value})")
 
         # Sign the transaction with your private key
         signed_tx = web3.eth.account.signTransaction(tx, your_wallet_address_private_key)
@@ -201,12 +201,12 @@ for i in range(1, num_of_xenft_to_mint+1):
         gas_price = web3.toWei(maxFeePerGas, 'gwei')
         cost = gas * gas_price
         cost_in_ether = float(round(web3.fromWei(cost, 'ether'),6))
-        total_cost_in_usd = round(cost_in_ether * eth_cost_in_usd,2)
+        total_cost_in_usd = round(cost_in_ether * matic_cost_in_usd, 2)
         per_vmu_cost = round(total_cost_in_usd/vmu,2)
-        print(f"{get_timestamp()} - Expected XENFT Cost: {cost_in_ether} ETH (${total_cost_in_usd}) or ${per_vmu_cost}/VMU. Current ETH value: ${eth_cost_in_usd}.")
+        print(f"{get_timestamp()} - Expected XENFT Cost: {cost_in_ether} MATIC (${total_cost_in_usd}) or ${per_vmu_cost}/VMU. Current MATIC value: ${matic_cost_in_usd}.")
 
         # Print transaction hash URL
-        print(f"{get_timestamp()} - XENFT (vmu:{vmu}, term:{term}) - Successfully Initiated at {maxFeePerGas} gwei. URL: https://etherscan.io/tx/" + str(web3.toHex(tx_hash)))
+        print(f"{get_timestamp()} - XENFT (vmu:{vmu}, term:{term}) - Successfully Initiated at {maxFeePerGas} gwei. URL: https://polygonscan.com/tx/" + str(web3.toHex(tx_hash)))
 
         # Wait for the transaction to be mined
         # tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
@@ -219,13 +219,13 @@ for i in range(1, num_of_xenft_to_mint+1):
 
         balance_after_claim = round(float(web3.fromWei(web3.eth.getBalance(your_wallet_address), 'ether')),6)
         now = datetime.datetime.now()
-        print(f"{get_timestamp()} - XENFT (vmu:{vmu}, term:{term}) - Successfully Created at {maxFeePerGas} gwei. URL: https://etherscan.io/tx/" + str(web3.toHex(tx_hash)))
+        print(f"{get_timestamp()} - XENFT (vmu:{vmu}, term:{term}) - Successfully Created at {maxFeePerGas} gwei. URL: https://polygonscan.com/tx/" + str(web3.toHex(tx_hash)))
         total_cost = float(round(balance_before_claim - balance_after_claim, 4))
-        total_cost_in_usd = round((total_cost * eth_cost_in_usd),2)
-        per_vmu_cost = round(total_cost_in_usd/vmu, 2)
-        total_account_value = round(eth_cost_in_usd*balance_after_claim,2)
-        print(f"{get_timestamp()} - Actual (final) XENFT Cost: {total_cost} ETH (${total_cost_in_usd}) or ${per_vmu_cost}/VMU. Current ETH value: ${eth_cost_in_usd}.")
-        print(f"{get_timestamp()} - Account balance after XENFT claim: {balance_after_claim} ETH (${total_account_value})")
+        total_cost_in_usd = round((total_cost * matic_cost_in_usd), 2)
+        per_vmu_cost = round(total_cost_in_usd / vmu, 2)
+        total_account_value = round(matic_cost_in_usd * balance_after_claim, 2)
+        print(f"{get_timestamp()} - Actual (final) XENFT Cost: {total_cost} MATIC (${total_cost_in_usd}) or ${per_vmu_cost}/VMU. Current MATIC value: ${matic_cost_in_usd}.")
+        print(f"{get_timestamp()} - Account balance after XENFT claim: {balance_after_claim} MATIC (${total_account_value})")
         print("-----------------------------------")
 
         ## ----------- BODY OF THE PROGRAM - END ----------- ##
